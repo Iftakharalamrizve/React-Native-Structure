c518ae52f53650fbfae98427a0973a7c5223b280
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-09-24 01:37:48 +0600

    child to parent and parent to child communication

--- src/App.js
+++ src/App.js
@@ -3,18 +3,41 @@ import Choice from "./Choice";
 import Details from "./Details";
 import faker from "faker";
 
-const App = () => {
-  return (
-    <div>
-      <Choice>
-        <Details
-          author="Sam"
-          timeAgo="Today at 4:45PM"
-          content="Nice blog post"
-          avatar={faker.image.avatar()}
-        />
-      </Choice>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      HOMELINK: "INitial State",
+    };
+  }
+  getAlert() {
+    alert("dsfdsgsdfsdfsdfsd");
+  }
+
+  changeHomeLink(name) {
+    this.setState({
+      HOMELINK: name,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>{this.state.HOMELINK}</h3>
+        <Choice
+          alertFuntion={this.getAlert}
+          changeHomeInfo={this.changeHomeLink.bind(this)}
+        >
+          <Details
+            author="Sam"
+            timeAgo="Today at 4:45PM"
+            content="Nice blog post"
+            avatar={faker.image.avatar()}
+          />
+        </Choice>
+      </div>
+    );
+  }
+}
+
 export default App;
--- src/Choice.js
+++ src/Choice.js
@@ -1,16 +1,44 @@
 import React from "react";
-const Choice = (props) => {
-  return (
-    <div className="ui card">
-      <div className="content">{props.children}</div>
-      <div className="extra content">
-        <div className="ui two buttons">
-          <div className="ui basic green button">Approve</div>
-          <div className="ui basic red button">Reject</div>
+class Choice extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      HomeLink: "",
+    };
+  }
+  onClickHome() {
+    this.props.changeHomeInfo(this.state.HomeLink);
+  }
+  render() {
+    return (
+      <div className="ui card">
+        <div className="content">{this.props.children}</div>
+        <div className="extra content">
+          <div className="ui two buttons">
+            <input
+              type="text"
+              value={this.state.HomeLink}
+              onChange={(e) => {
+                this.setState({ HomeLink: e.target.value });
+              }}
+            />
+            <div
+              className="ui basic green button"
+              onClick={this.onClickHome.bind(this)}
+            >
+              Approve
+            </div>
+            <div
+              onClick={this.props.alertFuntion}
+              className="ui basic red button"
+            >
+              Reject
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Choice;
