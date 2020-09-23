f9f2e9c914dc56d3fdde1c4e2718b118de2a855a
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-09-24 03:34:26 +0600

    List Rendering

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 import React from "react";
 import Choice from "./Choice";
-import Details from "./Details";
-import faker from "faker";
-
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      HOMELINK: "INitial State",
-    };
-  }
-  getAlert() {
-    alert("dsfdsgsdfsdfsdfsd");
-  }
-
-  changeHomeLink(name) {
-    this.setState({
-      HOMELINK: name,
-    });
-  }
-
   render() {
+    var infoList = [
+      {
+        title:
+          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body:
+          "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+      },
+      {
+        title: "qui est esse",
+        body:
+          "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+      },
+      {
+        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        body:
+          "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+      },
+    ];
+
+    var InfoListDom = infoList.map((item, i) => {
+      return <Choice dataList={item} key={i} />;
+    });
     return (
       <div>
-        <h3>{this.state.HOMELINK}</h3>
-        <Choice
-          alertFuntion={this.getAlert}
-          changeHomeInfo={this.changeHomeLink.bind(this)}
-        >
-          <Details
-            author="Sam"
-            timeAgo="Today at 4:45PM"
-            content="Nice blog post"
-            avatar={faker.image.avatar()}
-          />
-        </Choice>
+        <ul className="ui list">{InfoListDom}</ul>
       </div>
     );
   }
--- src/Choice.js
+++ src/Choice.js
@@ -1,43 +1,7 @@
 import React from "react";
 class Choice extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      HomeLink: "",
-    };
-  }
-  onClickHome() {
-    this.props.changeHomeInfo(this.state.HomeLink);
-  }
   render() {
-    return (
-      <div className="ui card">
-        <div className="content">{this.props.children}</div>
-        <div className="extra content">
-          <div className="ui two buttons">
-            <input
-              type="text"
-              value={this.state.HomeLink}
-              onChange={(e) => {
-                this.setState({ HomeLink: e.target.value });
-              }}
-            />
-            <div
-              className="ui basic green button"
-              onClick={this.onClickHome.bind(this)}
-            >
-              Approve
-            </div>
-            <div
-              onClick={this.props.alertFuntion}
-              className="ui basic red button"
-            >
-              Reject
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <li>{this.props.dataList.title}</li>;
   }
 }
 
