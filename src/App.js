8a035c5e5c82f10efa1ac78d2f8039f48660e22c
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-09-25 17:21:41 +0600

    HOC2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import React from "react";
-import Hover from "./Hover";
-import Click from "./Click";
-
 class App extends React.Component {
   render() {
-    return (
-      <React.Fragment>
-        <Click />
-        <Hover />
-      </React.Fragment>
-    );
+    return <React.Fragment></React.Fragment>;
   }
 }
 
