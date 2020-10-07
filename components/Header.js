a6489dda96586193eb4a139bd290de583d7055ad
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-10-07 18:04:33 +0600

    Update axios option

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
+import http from "../Helpers/http";
 import { StyleSheet, Text, View , Dimensions, TextInput ,Image, TouchableOpacity} from 'react-native';
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            item: "",
+        };
+    }
     render() {
+        const searchItem = async () => {
+            try {
+                const response = await http.get("/search/photos", {
+                params: { query: this.state.item },
+                });
+                this.props.onSetItem(response.data.results);
+                this.setState({ item: "" });
+            } catch (error) {}
+        };
+
         return <View style={styles.container}>
             <View style={styles.logoSection}>
                 <Image style={styles.logo} source={require("../assets/logo2.png")} />
             </View>
             <View style={styles.searchSection}>
-                <TextInput style={styles.searchBox} placeholder="Search Item..." />
+                <TextInput style={styles.searchBox} placeholder="Search Item..."  value={this.state.itemName} onChangeText={(item) => this.setState({ item })}/>
                 <TouchableOpacity>
                     <Image style={styles.searchIcon} source={require("../assets/search.png")} />
                 </TouchableOpacity>
