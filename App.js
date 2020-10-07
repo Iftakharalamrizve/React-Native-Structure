8735fcfcebbee1417c6c930075b4e15e7cce7f88
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-10-07 17:52:07 +0600

    List Structure Ready

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header'
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Header />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
