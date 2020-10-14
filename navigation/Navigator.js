5c580579198151c88c09b3dc1f65dd1b10829310
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-10-15 04:02:19 +0600

    initialize navigation project

---
+++ navigation/Navigator.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+class Navigator extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return ;
+    }
+}
