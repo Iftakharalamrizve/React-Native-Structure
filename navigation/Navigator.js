83b8c567fa22a3ed5720c756f4522b6e1979fd51
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-10-27 21:47:04 +0600

    update information

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
-import BottomTabBar from './BottomTabBar'
 import Drawer from './Drawer'
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 export default class Navigator extends Component {
     render() {
-        const Stack = createStackNavigator();
         return (
             <NavigationContainer>
                 <Drawer />
